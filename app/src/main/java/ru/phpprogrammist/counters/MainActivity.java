6a14dafd4e6f86baf7fbc5b82cf886f42a085966
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-11-10 18:14:50 +0300

    init

---
+++ app/src/main/java/ru/phpprogrammist/counters/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.phpprogrammist.counters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
