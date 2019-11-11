ba8871517a0563b917362e742ff98468f9601cb6
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-11-11 09:59:55 +0300

    Выбор даты с помощью DatePickerDialog

--- app/src/main/java/ru/phpprogrammist/counters/RecordActivity.java
+++ app/src/main/java/ru/phpprogrammist/counters/RecordActivity.java
@@ -6,17 +6,25 @@ import androidx.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.DatePicker;
+
 import android.widget.EditText;
+import android.widget.TextView;
 
 
+
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
-public class RecordActivity extends AppCompatActivity {
+public class RecordActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     private EditText editTextReadings;
-    private DatePicker datePicker;
+    private TextView textViewDate;
+    private Date selectedDate;
+    private DatePickerDialog dpd;
     private MainViewModel viewModel;
 
     @Override
@@ -25,23 +33,45 @@ public class RecordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_record);
         viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         editTextReadings = findViewById(R.id.editTextReadings);
-        datePicker = findViewById(R.id.datePicker);
-        datePicker.setCalendarViewShown(false);
-        datePicker.setSpinnersShown(true);
+        textViewDate = findViewById(R.id.textViewDate);
+
+        Calendar now = Calendar.getInstance();
+        dpd = DatePickerDialog.newInstance(
+                RecordActivity.this,
+                now.get(Calendar.YEAR), // Initial year selection
+                now.get(Calendar.MONTH), // Initial month selection
+                now.get(Calendar.DAY_OF_MONTH) // Inital day selection
+        );
+        selectedDate = now.getTime();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM YYYY", Locale.getDefault());
+        String date = dateFormat.format(selectedDate);
+        textViewDate.setText(date);
     }
 
 
     public void onClickSaveRecord(View view) {
         int readings = Integer.parseInt(editTextReadings.getText().toString().trim());
-        int year = datePicker.getYear();
-        int month = datePicker.getMonth();
-        int day = datePicker.getDayOfMonth();
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year,month,day);
-        Date date = calendar.getTime();
-        Record record = new Record(date,readings);
+
+        Record record = new Record(selectedDate,readings);
         viewModel.insertRecord(record);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year,monthOfYear,dayOfMonth);
+        selectedDate = calendar.getTime();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM YYYY", Locale.getDefault());
+        String date = dateFormat.format(selectedDate);
+
+        textViewDate.setText(date);
+
+    }
+
+    public void onClickPickDate(View view) {
+        dpd.show(getSupportFragmentManager(), "Datepickerdialog");
     }
 }
