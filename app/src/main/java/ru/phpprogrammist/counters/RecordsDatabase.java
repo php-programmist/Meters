30d252bc0efd732c7a3c03abcdae07f12bd43005
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-11-11 14:55:41 +0300

    Добавил миграцию БД

--- app/src/main/java/ru/phpprogrammist/counters/RecordsDatabase.java
+++ app/src/main/java/ru/phpprogrammist/counters/RecordsDatabase.java
@@ -5,17 +5,26 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
-@Database(entities = {Record.class}, version = 1, exportSchema = false)
+@Database(entities = {Record.class}, version = 2, exportSchema = false)
 public abstract class RecordsDatabase extends RoomDatabase {
     private static RecordsDatabase database;
     private static final String DB_NAME = "records.db";
     private static final Object LOCK = new Object();
-
+    public static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(final SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE records ADD COLUMN type INTEGER DEFAULT 1 NOT NULL");
+        }
+    };
     public static RecordsDatabase getInstance(Context context) {
+
         synchronized (LOCK) {
             if (database == null) {
                 database = Room.databaseBuilder(context, RecordsDatabase.class, DB_NAME)
+                        .addMigrations(RecordsDatabase.MIGRATION_1_2)
                         .build();
             }
         }
